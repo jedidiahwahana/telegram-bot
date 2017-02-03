aab5699d821dc886ed7776ae6c39b17588762c94
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-02-03 15:37:42 +0700

    feat: send text message to User

--- src/main/java/com/linecorp/example/telegrambot/BotController.java
+++ src/main/java/com/linecorp/example/telegrambot/BotController.java
@@ -13,6 +13,12 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.HttpClient;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+
+import java.util.*;
+import org.json.JSONObject;
 
 @RestController
 @RequestMapping(value="/telegrambot")
@@ -26,6 +32,39 @@ public class BotController
         System.out.println("Webhook received");
         
         System.out.println("Update: " + aUpdate);
+        
+        JSONObject jUpdate = new JSONObject(aUpdate);
+        JSONObject jMessage = new JSONObject(jUpdate.get("message"));
+        String userText = jMessage.get("text").toString();
+        JSONObject jChat = new JSONObject(jMessage.get("chat"));
+        String chatId = jChat.get("id").toString();
+        
+        String url = "https://api.telegram.org/"+tToken+"/sendMessage";
+        
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost post = new HttpPost(url);
+        
+        List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
+        urlParameters.add(new BasicNameValuePair("chat_id", chatId));
+        urlParameters.add(new BasicNameValuePair("text", userText));
+        
+        try{
+            post.setEntity(new UrlEncodedFormEntity(urlParameters));
+            HttpResponse response = client.execute(post);
+            System.out.println("Response Code : "
+                               + response.getStatusLine().getStatusCode());
+            
+            BufferedReader rd = new BufferedReader(
+                                                   new InputStreamReader(response.getEntity().getContent()));
+            
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+        } catch(IOException e){
+            System.out.println("Exception is raised");
+        }
         
         return new ResponseEntity<String>("Web is running", HttpStatus.OK);
     }
