1ca645851145a748f1e4073d05eb50a04c8f228c
Jedidiah Wahana <jedidiah.wahana@gmail.com>
2017-02-03 14:36:53 +0700

    Initial commit

---
+++ src/main/java/com/linecorp/example/telegrambot/BotController.java
@@ -0,0 +1,54 @@
+
+package com.linecorp.example.telegrambot;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient;
+import org.apache.http.HttpResponse;
+
+@RestController
+@RequestMapping(value="/telegrambot")
+public class BotController
+{
+    private final String tToken = "277622307:AAHumppdwKqfWxqgXzddyUukeTrF_zCNkWs";
+    
+    @GetMapping(value="/callback")
+    public ResponseEntity<String> callback()
+    {
+        System.out.println("Webhook received");
+        
+        String url = "https://api.telegram.org/bot"+tToken+"/getupdates";
+        
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        
+        try{
+            HttpResponse response = client.execute(request);
+            
+            System.out.println("Response Code : "
+                               + response.getStatusLine().getStatusCode());
+            
+            BufferedReader rd = new BufferedReader(
+                                                   new InputStreamReader(response.getEntity().getContent()));
+            
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            System.out.println("Result: " + result);
+        } catch(IOException e) {
+            System.out.println("Exception raised");
+        }
+        
+        return new ResponseEntity<String>("Web is running", HttpStatus.OK);
+    }
+};
